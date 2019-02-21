4e5d8f716ca7290a80775f1beaba2360c4dce2e3
Christopher Donner <christopher.donner@gmail.com>
2019-02-21 14:36:44 -0500

    survey page up

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function(){
     console.log(`listening on ${PORT}`)
