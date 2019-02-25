d9b7a2701e5166b278cfc2ad0ff1df4acc3d1bcc
Christopher Donner <christopher.donner@gmail.com>
2019-02-25 16:02:27 -0500

    post returns 404

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,13 +8,8 @@ module.exports=function(app){
 
     app.post("api/friends", function(req, res){
         console.log("post")
-        friendData.push(res)
-        for(i=0;i<friendData.length;i++){
-            var totalDifference
-            for(j=0;j<10;j++){
-                friendData.scores[i]
-            }
-        }
-        
+        console.log(res)
+        // friendData.push(res.json())
+        // res.json({ ok: true });
     })
 }
