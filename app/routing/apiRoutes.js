46221fd06bf21e9f0ad5cf2282b265710aadda7e
Christopher Donner <christopher.donner@gmail.com>
2019-02-26 13:15:43 -0500

    post working!

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,24 @@
-var friendData=require("../data/friends.js");
+var friendData=require("../data/friends");
+var friendArray=[]
 
 module.exports=function(app){
 
     app.get("/api/friends", function(req, res){
         res.json(friendData)
+    });
+
+    app.post("/api/friends", function(req, res){
+        console.log("post")
+        // console.log(res)
+        friendArray.push(friendData)
+        friendArray.push(res)
+        // res.json(friendArray);
+            for(i=0;i<friendArray.length;i++){
+                for(j=0;j<friendArray[i].score[j];i++){
+                    
+                }
+            }
+        res.json(true);
     })
 
-    app.post("api/friends", function(req, res){
-        console.log("post")
-        console.log(res)
-        // friendData.push(res.json())
-        // res.json({ ok: true });
-    })
 }
