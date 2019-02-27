6ac73769b6609e4a1f8f4d69e7ee7cec4b81bec0
christopherdonner <christopher.donner@gmail.com>
2019-02-27 00:16:22 -0500

    last push for the day

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,34 +7,37 @@ module.exports = function (app) {
         res.json(friendData)
     });
 
-    app.post("/api/friends", function (req, res) {
-        console.log("post")
-        // console.log(res)
+    app.post("/api/friends", function (req, res) 
+    {
         friendArray.push(friendData)
         friendArray.push(req.body)
-        console.log(friendArray)
-        console.log(friendArray[0].scores[0])
         res.friendArray;
         var temp
         var currentFriendVariance = 0
-        var tempfriendVariance = []
+        var tempfriendVariance = 0
         var matchName
         var matchPhoto
-        for (i = 0; i < friendArray.length; i++) {
-            for (j = 0; j < friendArray[i].scores[j]; i++) {
-                if (friendArray[i].scores[j] != req.body.scores[j]) {
+        for (var i = 0; i < friendArray.length; i++) 
+        {
+            console.log(friendArray[i].scores.length)
+            for (j = 0; j < 10; i++) 
+            {
+                console.log(friendArray[i].scores[j])
+                if (friendArray[i].scores[j] != req.body.scores[j]) 
+                {
                     temp = friendArray[i].scores[j] - req.body.scores[j]
                     if (temp < 0) { temp = temp * (-1) }
-                    tempfriendVariance[i] += temp
+                    tempfriendVariance = tempfriendVariance+temp
                 }
-                if (tempFriendVariance > currentFriendVariance) {
+                if (tempfriendVariance > currentFriendVariance) 
+                {
                     matchName = req.body.name;
                     matchPhoto = req.body.photo;
                 }
             }
-            res.json(true);
+            res.json({name: matchName,photo:matchPhoto});
             return;
-
         }
 
 })
+}
