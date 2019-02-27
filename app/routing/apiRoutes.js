64dd6a7bedb6ebfb24bf5e8f777716871f257d2c
christopherdonner <christopher.donner@gmail.com>
2019-02-26 23:59:40 -0500

    eleventh hour

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,13 @@
-var friendData=require("../data/friends");
-var friendArray=[]
+var friendData = require("../data/friends");
+var friendArray = []
 
-module.exports=function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res){
+    app.get("/api/friends", function (req, res) {
         res.json(friendData)
     });
 
-    app.post("/api/friends", function(req, res){
+    app.post("/api/friends", function (req, res) {
         console.log("post")
         // console.log(res)
         friendArray.push(friendData)
@@ -16,19 +16,25 @@ module.exports=function(app){
         console.log(friendArray[0].scores[0])
         res.friendArray;
         var temp
-        var friendVariance=[]
-        for(i=0;i<friendArray.length;i++){
-                for(j=0;j<friendArray[i].scores[j];i++){
-                    if(friendArray[i].scores[j]!=req.body.scores[j]){
-                        temp=friendArray[i].scores[j]-req.body.scores[j]
-                        if(temp<0){temp=temp*(-1)}
-                        friendVariance[i]+=temp    
-                    }
+        var currentFriendVariance = 0
+        var tempfriendVariance = []
+        var matchName
+        var matchPhoto
+        for (i = 0; i < friendArray.length; i++) {
+            for (j = 0; j < friendArray[i].scores[j]; i++) {
+                if (friendArray[i].scores[j] != req.body.scores[j]) {
+                    temp = friendArray[i].scores[j] - req.body.scores[j]
+                    if (temp < 0) { temp = temp * (-1) }
+                    tempfriendVariance[i] += temp
+                }
+                if (tempFriendVariance > currentFriendVariance) {
+                    matchName = req.body.name;
+                    matchPhoto = req.body.photo;
                 }
             }
-        res.json(true);
-        return;
+            res.json(true);
+            return;
 
-    })
+        }
 
-}
+})
