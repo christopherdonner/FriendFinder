ffd34ed827767da9a269e51c2952d84bc1ed3931
christopherdonner <43712817+christopherdonner@users.noreply.github.com>
2019-02-27 19:46:37 -0500

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,26 +7,23 @@ module.exports = function (app) {
         res.json(friendData)
     });
 
-    app.post("/api/friends", function (req, res) 
+    app.post("/api/friends", function (req, res)  // this is the post
     {
-        friendArray.push(friendData)
+        friendArray.push(friendData)        // this is the call back that is failing, causing the HTTP 500 response
         friendArray.push(req.body)
         res.friendArray;
-        console.log(friendArray)
         var temp
         var currentFriendVariance = 0
         var tempfriendVariance = 0
         var matchName
         var matchPhoto
-        console.log(req.body.scores[0])
         for (var i = 0; i < friendArray.length; i++) 
         {
             console.log(friendArray[i].scores.length)
             for (j = 0; j < 10; i++) 
             {
-                console.log(friendArray[i].scores[j])
-                console.log(req.body)
-                if (friendArray[i].scores[j] !== req.body.scores[j]) 
+                console.log(friendArray[i].scores[j])   // this console log appears to come back as an integer value
+                if (friendArray[i].scores[j] != req.body.scores[j]) // however, this returns undefined, causing the crash
                 {
                     temp = friendArray[i].scores[j] - req.body.scores[j]
                     if (temp < 0) { temp = temp * (-1) }
